e19d5886c86259df5f826644ba545da5660f6f18
shenqing <shenqin1@msu.edu>
2023-03-18 15:46:18 -0400

    Initial Commit

---
+++ BudgetFormatter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudgetFormatter
+//
+//  Created by 沈清昊 on 3/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
