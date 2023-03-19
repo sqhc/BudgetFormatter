8c44c169914848169dfdc2bd778f4c6a13e5a74a
shenqing <shenqin1@msu.edu>
2023-03-19 09:47:59 -0400

    The second test file, for the budget calculation.

---
+++ BudgetFormatterTests/BudgetTests.swift
@@ -0,0 +1,28 @@
+//
+//  BudgetTests.swift
+//  BudgetFormatterTests
+//
+//  Created by 沈清昊 on 3/19/23.
+//
+
+import XCTest
+@testable import BudgetFormatter
+
+class BudgetTests: XCTestCase {
+
+    func testBudgetContainsWeeklyRemaining(){
+        
+    }
+    
+    func testBudgetContainsDailyRemaining(){
+        
+    }
+    
+    func testTransactionDeductsFromWeeklyRemaining(){
+        
+    }
+    
+    func testTransactionDeductsFromDailyRemaining(){
+        
+    }
+}
